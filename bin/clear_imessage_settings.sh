a2671060654b013a7b16c92fbe35ec91cc12323c
Brendan Erwin <brendanjerwin@gmail.com>
2014-11-16 09:15:40 -0500

    imessage hacking

---
+++ bin/clear_imessage_settings.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+rm -rf ~/Library/Caches/com.apple.Messages
+rm -rf ~/Library/Caches/com.apple.imfoundation.IMRemoteURLConnectionAgent
+
+rm ~/Library/Preferences/com.apple.iChat*
+rm ~/Library/Preferences/com.apple.imagent.*
+rm ~/Library/Preferences/com.apple.imessage.*
+rm ~/Library/Preferences/com.apple.imservice.*
+rm ~/Library/Preferences/com.apple.security.*
+
+rm -rf ~/Library/Messages
